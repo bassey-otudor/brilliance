805bacb7cd783d9cfebb557791ae91b14e26d71c
Bassey E. Otudor <bassey.otudor@outlook.com>
2024-03-12 18:37:27 +0100

    Added 'remember me' feature

--- src/main/java/learn/brilliance/Controller/Admin/AdminMenuController.java
+++ src/main/java/learn/brilliance/Controller/Admin/AdminMenuController.java
@@ -3,6 +3,7 @@ package learn.brilliance.Controller.Admin;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import learn.brilliance.Model.Model;
 import learn.brilliance.View.Enums.AdminMenuOptions;
 
@@ -20,7 +21,7 @@ public class AdminMenuController implements Initializable {
     public Button admin_degreesBtn;
     public Button admin_minorsBtn;
     public Button admin_settingsBtn;
-    public Button admin_signoutBtn;
+    public Button admin_logoutBtn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -37,6 +38,8 @@ public class AdminMenuController implements Initializable {
         admin_degreesBtn.setOnAction(e -> onDegrees());
         admin_minorsBtn.setOnAction(e -> onMinors());
         admin_settingsBtn.setOnAction(e -> onSettings());
+        admin_logoutBtn.setOnAction(e -> onLogout()
+        );
     }
     private void onDashboard() {
         Model.getInstance().getViewFactory().getAdminSelectedMenuOption().set(AdminMenuOptions.DASHBOARD);
@@ -64,5 +67,12 @@ public class AdminMenuController implements Initializable {
     }
     private void onSettings() {
         Model.getInstance().getViewFactory().getAdminSelectedMenuOption().set(AdminMenuOptions.SETTINGS);
+    }
+    // Returns the user to the login window
+    private void onLogout() {
+        Stage stage = (Stage)admin_currentSection.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().setAdminLoginStatus(false);
+        Model.getInstance().getViewFactory().showLoginWindow();
     }
 }
--- src/main/java/learn/brilliance/Controller/LoginController.java
+++ src/main/java/learn/brilliance/Controller/LoginController.java
@@ -1,6 +1,5 @@
 package learn.brilliance.Controller;
 
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -8,12 +7,13 @@ import javafx.stage.Stage;
 import learn.brilliance.Model.Model;
 import learn.brilliance.View.Enums.AccountType;
 
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
     public TextField loginID;
-    public TextField password;
+    public PasswordField password;
     public CheckBox rememberMe;
     public Hyperlink forgotPwd;
     public Button loginBtn;
@@ -30,27 +30,52 @@ public class LoginController implements Initializable {
         acc_selector.setValue(Model.getInstance().getViewFactory().getAccountType());
         acc_selector.valueProperty().addListener(observable -> setAcc_selector());
         loginBtn.setOnAction(e -> onLogin());
+        getRemember();
+        rememberMe.selectedProperty().addListener((observableValue, oldVal, newVal) -> {
+             if(newVal) {
+                 setRememberMe();
+             } else {
+                 removeRememberMe();
+             }
+         });
+
     }
 
     // Login control function
     private void onLogin() {
         Stage stage = (Stage)errorLabel.getScene().getWindow();
+        inputValidator(stage);
+    }
 
-        if(Model.getInstance().getViewFactory().getAccountType() == AccountType.ADMIN) {
-            Model.getInstance().evaluateAdminLogin(loginID.getText(), password.getText());
+    // Makes sure all fields are filled
+    private void inputValidator(Stage stage) {
+        if(loginID.getText().isEmpty() && password.getText().isEmpty()) {
+            loginID.setStyle("-fx-border-color: #EC6666 ; -fx-border-width: 1px ;");
+            password.setStyle("-fx-border-color: #EC6666 ; -fx-border-width: 1px ;");
 
-            if (Model.getInstance().getAdminLoginStatus()) {
-                Model.getInstance().getViewFactory().closeStage(stage);
-                Model.getInstance().getViewFactory().showAdminWindow();
-            } else {
-                loginID.setText("");
-                password.setText("");
-                errorLabel.setText("Invalid login credentials");
-            }
-        } else if (Model.getInstance().getViewFactory().getAccountType() == AccountType.STUDENT) {
-            Model.getInstance().getViewFactory().showStudentWindow();
+        } else if (loginID.getText().isEmpty()) {
+            loginID.setStyle("-fx-border-color:  #EC6666 ; -fx-border-width: 1px ;");
+            password.setStyle("-fx-border-color: #40C5CF; -fx-border-width: 1px ;");
+
+        } else if (password.getText().isEmpty()) {
+            loginID.setStyle("-fx-border-color:  #40C5CF ; -fx-border-width: 1px ;");
+            password.setStyle("-fx-border-color: #EC6666 ; -fx-border-width: 1px ;");
+
         } else {
-            Model.getInstance().getViewFactory().showTeacherWindow();
+            if(Model.getInstance().getViewFactory().getAccountType() == AccountType.ADMIN) {
+                Model.getInstance().evaluateAdminLogin(loginID.getText(), password.getText());
+                if(Model.getInstance().getAdminLoginStatus()) {
+                    Model.getInstance().getViewFactory().closeStage(stage);
+                    Model.getInstance().getViewFactory().showAdminWindow();
+                } else {
+                    errorLabel.setText("Invalid Credentials. Please try again.");
+                }
+
+            } else if (Model.getInstance().getViewFactory().getAccountType() == AccountType.STUDENT) {
+                Model.getInstance().getViewFactory().showStudentWindow();
+            } else {
+                Model.getInstance().getViewFactory().showTeacherWindow();
+            }
         }
     }
 
@@ -60,11 +85,113 @@ public class LoginController implements Initializable {
         // Set the appropriate user prompt
         if(acc_selector.getValue() == AccountType.ADMIN) {
             loginID.setPromptText("Username");
+            clearFields();
+
         } else if (acc_selector.getValue() == AccountType.STUDENT) {
             loginID.setPromptText("Student ID");
+            clearFields();
+
         } else {
             loginID.setPromptText("Teacher ID");
+            clearFields();
         }
     }
+    private void getRemember() {
+        String filePath = "C:\\Users\\Bassey\\Documents\\Java Projects\\brilliance\\src\\main\\resources\\Settings\\settings.cfg";
+        File file = new File(filePath);
 
+        // check if the file exists and sets
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
+                // stores the contents of the read file in an array of strings.
+                String[] contents = reader.readLine().split("-");
+
+                if (contents[0].equals("true")) {
+                    this.rememberMe.setSelected(true);
+                    this.loginID.setText(contents[1]);
+                    switch (contents[2]) {
+                        case "ADMIN" -> acc_selector.setValue(AccountType.ADMIN);
+                        case "STUDENT" -> acc_selector.setValue(AccountType.STUDENT);
+                        case "TEACHER" -> acc_selector.setValue(AccountType.TEACHER);
+                    }
+                } else {
+                    acc_selector.setValue(AccountType.STUDENT);
+                }
+
+            } catch (IOException e) {
+                System.out.println("File not found.");
+                e.printStackTrace();
+            }
+
+        } else {
+            File settings = new File(filePath);
+            try {
+                if (settings.createNewFile()) {
+                    String[] contents = new String[3];
+                    contents[0] = String.valueOf(rememberMe.isSelected());
+                    contents[1] = loginID.getText();
+
+                    if(acc_selector.getValue().equals(AccountType.ADMIN)) {
+                        contents[2] = "ADMIN";
+                    } else if(acc_selector.getValue().equals(AccountType.STUDENT)) {
+                        contents[2] = "STUDENT";
+                    } else if(acc_selector.getValue().equals(AccountType.TEACHER)) {
+                        contents[2] = "TEACHER";
+                    }
+
+                    FileWriter writer = new FileWriter(filePath);
+                    writer.write(contents[0] + "-" + contents[1] + "-" + contents[2]);
+                    writer.close();
+                }
+
+            } catch (IOException e) {
+                System.out.println("Error creating file");
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    // Save the user's settings
+    private void setRememberMe() {
+        String filePath = "C:\\Users\\Bassey\\Documents\\Java Projects\\brilliance\\src\\main\\resources\\Settings\\settings.cfg";
+        String[] contents = new String[3];
+        if(loginID.getText().isEmpty()) {
+            loginID.setStyle("-fx-border-color: #EC6666 ; -fx-border-width: 1px ;");
+
+        } else{
+            contents[0] = String.valueOf(rememberMe.isSelected());
+            contents[1] = loginID.getText();
+            contents[2] = String.valueOf(acc_selector.getValue());
+
+            try {
+                FileWriter writer = new FileWriter(filePath, false);
+                writer.write(contents[0] + "-" + contents[1] + "-" + contents[2]);
+                writer.close();
+
+            } catch (Exception e) {
+                System.out.println("Error saving settings");
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void removeRememberMe() {
+        String filePath = "C:\\Users\\Bassey\\Documents\\Java Projects\\brilliance\\src\\main\\resources\\Settings\\settings.cfg";
+        try {
+            FileWriter writer = new FileWriter(filePath, false);
+            writer.write(String.valueOf(rememberMe.isSelected()));
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void clearFields() {
+        loginID.setText("");
+        password.setText("");
+    }
 }
