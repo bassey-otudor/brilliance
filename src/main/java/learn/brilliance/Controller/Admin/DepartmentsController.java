3da177c368a065fe6207fae6dea7a928ded6a6d1
Bassey E. Otudor <bassey.otudor@outlook.com>
2024-03-18 20:45:11 +0100

    Added add, delete, and update button functionality on the department section.

--- src/main/java/learn/brilliance/Controller/Admin/DepartmentsController.java
+++ src/main/java/learn/brilliance/Controller/Admin/DepartmentsController.java
@@ -2,6 +2,8 @@ package learn.brilliance.Controller.Admin;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import learn.brilliance.Model.Faculty;
+import learn.brilliance.Model.Model;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,17 +19,86 @@ public class DepartmentsController implements Initializable {
     public TableColumn dept_tableView_col_minor1;
     public TableColumn dept_tableView_col_minor2;
     public TextField dept_deptName;
-    public ChoiceBox dept_faculty;
-    public ChoiceBox dept_minor2;
-    public ChoiceBox dept_hod;
-    public ChoiceBox dept_minor1;
+    public ChoiceBox<String> dept_faculty;
+    public ChoiceBox<String> dept_minor2;
+    public ChoiceBox<String> dept_hod;
+    public ChoiceBox<String> dept_minor1;
     public Button dept_deleteBtn;
     public Button dept_clearBtn;
     public Button dept_updateBtn;
     public Button dept_addBtn;
+    public TextField dept_deptID;
+    public Label operationStatus;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        dept_faculty.setItems(Model.getInstance().getConnectDB().getFaculties());
+        dept_hod.setItems(Model.getInstance().getConnectDB().getHod());
+        dept_minor1.setItems(Model.getInstance().getConnectDB().getMinor1());
+        dept_minor2.setItems(Model.getInstance().getConnectDB().getMinor2());
+        dept_addBtn.setOnAction(e -> createDepartment());
+        dept_updateBtn.setOnAction(e -> updateDepartment());
+        dept_deleteBtn.setOnAction(e -> deleteDepartment());
+        dept_clearBtn.setOnAction(e -> clearFields());
+    }
 
+    private void createDepartment() {
+        String departmentID = dept_deptID.getId();
+        String departmentName = dept_deptName.getText();
+        String facultyID = dept_faculty.getSelectionModel().toString();
+        String hod = dept_hod.getSelectionModel().toString();
+        String minor1 = dept_minor1.getSelectionModel().toString();
+        String minor2 = dept_minor2.getSelectionModel().toString();
+
+        if(departmentID.isEmpty() || departmentName.isEmpty() || facultyID.isEmpty()) {
+            operationStatus.setStyle("-fx-text-fill: #EC6666; -fx-font-size: 1.0em;");
+            operationStatus.setText("Please fill required fields.");
+
+        } else {
+            Model.getInstance().getConnectDB().createDepartment(departmentID, departmentName, facultyID, hod, minor1, minor2);
+            operationStatus.setStyle("-fx-text-fill: green; -fx-font-size: 1em;");
+            operationStatus.setText("Department successfully added.");
+            clearFields();
+        }
+    }
+    private void updateDepartment() {
+        String departmentID = dept_deptID.getText();
+        String departmentName = dept_deptName.getText();
+        String facultyID = dept_faculty.getSelectionModel().toString();
+        String hod = dept_hod.getSelectionModel().toString();
+        String minor1 = dept_minor1.getSelectionModel().toString();
+        String minor2 = dept_minor2.getSelectionModel().toString();
+
+        if(departmentID.isEmpty() || departmentName.isEmpty() || facultyID.isEmpty()) {
+            operationStatus.setStyle("-fx-text-fill: #EC6666; -fx-font-size: 1.0em;");
+            operationStatus.setText("Please fill required fields.");
+
+        } else {
+            Model.getInstance().getConnectDB().updateDepartment(departmentID, departmentName, facultyID, hod, minor1, minor2);
+            operationStatus.setStyle("-fx-text-fill: green; -fx-font-size: 1em;");
+            operationStatus.setText("Department successfully updated.");
+            clearFields();
+        }
+
+    }
+    private void deleteDepartment() {
+        String departmentID = dept_deptID.getText();
+        if(departmentID.isEmpty()) {
+            operationStatus.setStyle("-fx-text-fill: #EC6666; -fx-font-size: 1.0em;");
+            operationStatus.setText("Please enter a department ID.");
+        } else {
+            Model.getInstance().getConnectDB().deleteDepartment(departmentID);
+            operationStatus.setStyle("-fx-text-fill: green; -fx-font-size: 1em;");
+            operationStatus.setText("Department successfully deleted.");
+            clearFields();
+        }
+    }
+    private void clearFields() {
+        dept_deptID.setText("");
+        dept_deptName.setText("");
+        dept_faculty.setValue("");
+        dept_hod.setValue("");
+        dept_minor1.setValue("");
+        dept_minor2.setValue("");
     }
 }
--- src/main/java/learn/brilliance/Model/connectDB.java
+++ src/main/java/learn/brilliance/Model/connectDB.java
@@ -88,7 +88,7 @@ public class connectDB {
     }
     public void updateFaculty(String facultyID,String facultyName, String director, String department1, String department2, String department3) {
         try {
-            Statement stmt = this.conn.createStatement();
+            Statement stmt = conn.createStatement();
             String update = "UPDATE faculties SET facultyID = '"+facultyID+"', facultyName ='"+facultyName+"', Director ='"+director+"', Department1 ='"+department1+"', Department2 ='"+department2+"', Department3 ='"+department3+"' WHERE facultyID ='"+facultyID+"';";
             stmt.executeUpdate(update);
         } catch (SQLException e) {
@@ -103,6 +103,135 @@ public class connectDB {
             stmt.executeUpdate(delete);
         } catch (SQLException e) {
             System.out.println("Error deleting faculty.");
+            e.printStackTrace();
+        }
+    }
+
+    // Department section
+    public ObservableList<String> getHod() {
+
+        List<String> hodList = new ArrayList<>();
+        Statement stmt;
+        ResultSet resultSet;
+
+        try {
+            String sql = "SELECT CONCAT(fname, ' ', lName) AS deptHod FROM teachers WHERE position = 'hod'";
+            stmt = conn.createStatement();
+            resultSet = stmt.executeQuery(sql);
+
+            while (resultSet.next()) {
+                hodList.add(resultSet.getString("departmentHod"));
+            }
+
+            hodList = FXCollections.observableArrayList(hodList);
+
+        } catch (SQLException e) {
+            System.out.println("Error retrieving HODs");
+            e.printStackTrace();
+        }
+
+        assert hodList instanceof ObservableList<String>;
+        return (ObservableList<String>) hodList;
+    }
+    public ObservableList<String> getFaculties() {
+        List<String> faculityList = new ArrayList<>();
+        Statement stmt;
+        ResultSet resultSet;
+
+        try {
+            String sql = "SELECT facultyID from faculties;";
+            stmt = conn.createStatement();
+            resultSet = stmt.executeQuery(sql);
+
+            while (resultSet.next()) {
+                faculityList.add(resultSet.getString("facultyID"));
+            }
+            faculityList = FXCollections.observableArrayList(faculityList);
+
+        } catch (SQLException e) {
+            System.out.println("Unable to get list of faculties.");
+            e.printStackTrace();
+        }
+
+        assert faculityList instanceof ObservableList<String>;
+        return (ObservableList<String>) faculityList;
+    }
+    public ObservableList<String> getMinor1() {
+        List<String> minor1List = new ArrayList<>();
+        Statement stmt;
+        ResultSet resultSet;
+
+        try {
+            String sql = "SELECT minorID from minors;";
+            stmt = conn.createStatement();
+            resultSet = stmt.executeQuery(sql);
+
+            while (resultSet.next()) {
+                minor1List.add(resultSet.getString("minor1"));
+            }
+            minor1List = FXCollections.observableArrayList(minor1List);
+
+        } catch (SQLException e) {
+            System.out.println("Unable to get list of minor1.");
+            e.printStackTrace();
+        }
+
+        assert minor1List instanceof ObservableList<String>;
+        return (ObservableList<String>) minor1List;
+    }
+    public ObservableList<String> getMinor2() {
+        List<String> minor2List = new ArrayList<>();
+        Statement stmt;
+        ResultSet resultSet;
+
+        try {
+            String sql = "SELECT minorID from minors;";
+            stmt = conn.createStatement();
+            resultSet = stmt.executeQuery(sql);
+
+            while (resultSet.next()) {
+                minor2List.add(resultSet.getString("minor2"));
+            }
+            minor2List = FXCollections.observableArrayList(minor2List);
+
+        } catch (SQLException e) {
+            System.out.println("Unable to get list of minors2.");
+            e.printStackTrace();
+        }
+
+        assert minor2List instanceof ObservableList<String>;
+        return (ObservableList<String>) minor2List;
+    }
+    public void createDepartment(String departmentID, String departmentName, String facultyID, String hod, String minor1, String minor2) {
+        Statement stmt;
+        try{
+            stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO departments "
+                    + "(deptID, deptName, facultyID, hod, minor1, minor2)"
+                    + "VALUES ('"+ departmentID +"', '"+ departmentName +"', '"+facultyID+"', '"+hod+"', '"+minor1+"', '"+minor2+"')");
+
+        } catch (SQLException e) {
+            System.out.println("Unable to create department");
+            e.printStackTrace();
+        }
+    }
+    public void updateDepartment(String departmentID, String departmentName, String facultyID, String hod, String minor1, String minor2) {
+        try {
+            Statement stmt = conn.createStatement();
+            String update = "UPDATE departments SET deptID = '"+departmentID+"', deptName ='"+departmentName+"', facultyID ='"+facultyID+"', hod='"+hod+"',minor1='"+minor1+"', minor2='"+minor2+"' WHERE deptID ='"+departmentID+"';";
+            stmt.executeUpdate(update);
+        } catch (SQLException e) {
+            System.out.println("Unable to update department info.");
+            e.printStackTrace();
+        }
+    }
+    public void deleteDepartment(String departmentID) {
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("DELETE FROM departments WHERE deptID='"+departmentID+"';");
+
+        } catch (SQLException e) {
+            System.out.println("Unable to delete department.");
             e.printStackTrace();
         }
     }
